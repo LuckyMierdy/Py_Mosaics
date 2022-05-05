49ac83a7fa84f4f590299686aaa192f103258fca
Fabien GIBON <fabien.gibon@yahoo.fr>
2022-05-05 15:06:57 +0200

    modification user role json

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -20,7 +20,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
   #[ORM\Column(type: 'string', length: 180, unique: true)]
   private $email;
 
-  #[ORM\Column(type: 'string')]
+  #[ORM\Column(type: 'json')]
   private $roles = [];
 
   #[ORM\Column(type: 'string')]
