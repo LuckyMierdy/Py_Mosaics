1d104c646163ca9d98aa5d57d5cd254a0cbdae66
Fabien GIBON <fabien.gibon@yahoo.fr>
2022-05-05 14:25:56 +0200

    modification user entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,105 +9,105 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[UniqueEntity(fields: ['username'], message: 'There is already an account with this username')]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column(type: 'integer')]
-    private $id;
+  #[ORM\Id]
+  #[ORM\GeneratedValue]
+  #[ORM\Column(type: 'integer')]
+  private $id;
 
-    #[ORM\Column(type: 'string', length: 180, unique: true)]
-    private $username;
+  #[ORM\Column(type: 'string', length: 180, unique: true)]
+  private $email;
 
-    #[ORM\Column(type: 'json')]
-    private $roles = [];
+  #[ORM\Column(type: 'string')]
+  private $roles = [];
 
-    #[ORM\Column(type: 'string')]
-    private $password;
+  #[ORM\Column(type: 'string')]
+  private $password;
 
-    #[ORM\Column(type: 'string', length: 255)]
-    private $email;
+  #[ORM\Column(type: 'string', length: 255)]
+  private $username;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
 
-    public function getUsername(): ?string
-    {
-        return $this->username;
-    }
+  public function getUsername(): ?string
+  {
+    return $this->username;
+  }
 
-    public function setUsername(string $username): self
-    {
-        $this->username = $username;
+  public function setUsername(string $username): self
+  {
+    $this->username = $username;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    /**
-     * A visual identifier that represents this user.
-     *
-     * @see UserInterface
-     */
-    public function getUserIdentifier(): string
-    {
-        return (string) $this->username;
-    }
+  /**
+   * A visual identifier that represents this user.
+   *
+   * @see UserInterface
+   */
+  public function getUserIdentifier(): string
+  {
+    return (string) $this->username;
+  }
 
-    /**
-     * @see UserInterface
-     */
-    public function getRoles(): array
-    {
-        $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+  /**
+   * @see UserInterface
+   */
+  public function getRoles(): array
+  {
+    $roles = $this->roles;
+    // guarantee every user at least has ROLE_USER
+    $roles[] = 'ROLE_USER';
 
-        return array_unique($roles);
-    }
+    return array_unique($roles);
+  }
 
-    public function setRoles(array $roles): self
-    {
-        $this->roles = $roles;
+  public function setRoles(array $roles): self
+  {
+    $this->roles = $roles;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    /**
-     * @see PasswordAuthenticatedUserInterface
-     */
-    public function getPassword(): string
-    {
-        return $this->password;
-    }
+  /**
+   * @see PasswordAuthenticatedUserInterface
+   */
+  public function getPassword(): string
+  {
+    return $this->password;
+  }
 
-    public function setPassword(string $password): self
-    {
-        $this->password = $password;
+  public function setPassword(string $password): self
+  {
+    $this->password = $password;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    /**
-     * @see UserInterface
-     */
-    public function eraseCredentials()
-    {
-        // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
-    }
+  /**
+   * @see UserInterface
+   */
+  public function eraseCredentials()
+  {
+    // If you store any temporary, sensitive data on the user, clear it here
+    // $this->plainPassword = null;
+  }
 
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
+  public function getEmail(): ?string
+  {
+    return $this->email;
+  }
 
-    public function setEmail(string $email): self
-    {
-        $this->email = $email;
+  public function setEmail(string $email): self
+  {
+    $this->email = $email;
 
-        return $this;
-    }
+    return $this;
+  }
 }
