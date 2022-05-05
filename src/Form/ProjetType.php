220dca218a50a719db6bca1f90abd27c4f541dcc
Fabien GIBON <fabien.gibon@yahoo.fr>
2022-05-05 10:35:55 +0200

    Ajout de la page login et s'enregistrer

---
+++ src/Form/ProjetType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Projet;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProjetType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('image')
+            ->add('image2')
+            ->add('image3')
+            ->add('image4')
+            ->add('image5')
+            ->add('image6')
+            ->add('image7')
+            ->add('description2')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Projet::class,
+        ]);
+    }
+}
