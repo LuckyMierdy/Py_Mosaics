2aac98bbd6c07e1ae1c55be5d1ee330305a830eb
Fabien GIBON <fabien.gibon@yahoo.fr>
2022-05-11 11:18:44 +0200

    Ajout page Voir les messages

--- src/Controller/ShowMessageController.php
+++ src/Controller/ShowMessageController.php
@@ -2,17 +2,18 @@
 
 namespace App\Controller;
 
+use App\Repository\MessageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ShowMessageController extends AbstractController
 {
-    #[Route('/show/message', name: 'app_show_message')]
-    public function index(): Response
-    {
-        return $this->render('show_message/index.html.twig', [
-            'controller_name' => 'ShowMessageController',
-        ]);
-    }
+  #[Route('/showMessage', name: 'app_show_message')]
+  public function index(MessageRepository $messageRepository): Response
+  {
+    return $this->render('show_message/index.html.twig', [
+      'messages' => $messageRepository->findAll(),
+    ]);
+  }
 }
